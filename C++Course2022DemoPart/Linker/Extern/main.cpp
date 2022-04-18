a2cec2b0c57eb01f4c4ace547f5d2abce3e372ba
Ahmed Ibrahim <ahmedyassinabdelwahed@gmail.com>
2022-04-18 19:05:03 +0200

    new change

--- C++Course2022DemoPart/Linker/Extern/main.cpp
+++ C++Course2022DemoPart/Linker/Extern/main.cpp
@@ -1,12 +1,9 @@
 #include <iostream>
-
 using namespace std;
-extern int f1(void);
 
-int main()
-{
-    int x;
-    x=10*f1();
-    cout<<x<<endl;
-    return 0;
+extern int f1(void);
+int main() {
+    int x=10 *f1();
+   cout << "Value of X" << x <<endl; // This prints Hello, World!
+   return 0;
 }
